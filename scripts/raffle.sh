eccdf40dddc127a721398d52a6f7c311843b569b
Leonardo Rebouças de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2021-03-29 16:41:30 -0300

    Test blocks

--- scripts/raffle.sh
+++ scripts/raffle.sh
@@ -1,15 +1,39 @@
+#/bin/bash
+if [ "${1}" == "" ]; then
+  echo "Block size missing"
+  return 1
+fi
+
+BLOCKS=$1
 TESTCASES_PATH=../sequences/test_cases_json
 TESTCASES_TARGET=../sequences/test_cases_blocks
-TESTS_LENGTH=$(seq 10)
+TESTS_LENGTH=$(seq ${BLOCKS})
 MAX_CASE=$(ls -la ${TESTCASES_PATH} | wc -l)
 MAX_CASE=$((MAX_CASE - 3))
+
+
+getNumber(){
+  N=false
+  while [ "${N}" == "false" ];do
+    N=$(shuf -i 1-${MAX_CASE} -n 1)
+    for i in ${NUMBERLIST};do
+      if [ "$i" == "$N" ]; then
+        N=false
+      fi
+    done
+  done
+  NUMBERLIST="${NUMBERLIST} ${N}"
+}
+
 for i in ${TESTS_LENGTH}; do
+  NUMBERLIST=""
   FOLDER=${TESTCASES_TARGET}/${i}
   mkdir -p $FOLDER
   CONT=1
   while [ ${CONT} -le ${i} ]; do
-    N=$(shuf -i 1-${MAX_CASE} -n 1)
-    cp ${TESTCASES_PATH}/${N}.json ${FOLDER}
+    getNumber
+    ln -s ../../${TESTCASES_PATH}/${N}.json ${FOLDER}/${N}.json
     CONT=$((CONT + 1))
   done
 done
+
